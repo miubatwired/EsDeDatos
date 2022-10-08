f82bc8ef7b563a89318a6fb5c63800e0a7eeec41
Leo <89736703+NexWan@users.noreply.github.com>
2022-10-08 18:09:53 -0500

    Add files via upload

---
+++ Unidad 2/PilaChar.java
@@ -0,0 +1,91 @@
+public class PilaChar {
+    private char p[];
+    private int tope;
+
+    public PilaChar() {
+        p=new char[5];
+        tope = -1;
+    }
+
+    public PilaChar(int n) {
+        p=new char[n];
+        tope=-1;
+    }
+
+    public char[] getP() {
+        return p;
+    }
+
+    public int getTope() {
+        return tope;
+    }
+
+    public void setP(char[] nuevoP) {
+        p = nuevoP;
+    }
+
+    public void setTope(int nuevoTope) {
+        tope = nuevoTope;
+    }
+
+    public void insertar(char nuevo) {
+        if(!estaLlena()) {
+            tope++;
+            p[tope] = nuevo;
+        }
+    }
+
+    public char eliminar() {
+        char borrado=' ';
+        if(!estaVacia()) {
+            borrado=p[tope];
+            p[tope]=' ';
+            tope--;
+        }
+        return borrado;
+    }
+
+    public boolean estaVacia() {
+        if(tope==-1)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean estaLlena() {
+        if(tope==p.length-1)
+            return true;
+        else
+            return false;
+    }
+
+    public char datoEnTope() {
+        if(!estaVacia())
+            return p[tope];
+        else
+            return 'a';
+    }
+
+    public String vaciar(){
+        String s = "";
+        for(int i =tope; i>0;i--){
+            s+=p[i];
+        }
+        return s;
+    }
+
+    public int numElementos() {
+        return tope+1;
+    }
+
+    public String toString() {
+        String s="Elementos de la pila:\n";
+        int i;
+        for(i=tope; i>=0; i--) {
+            s+=p[i]+"\n";
+        }
+        s+="Fin de la pila";
+        return s;
+    }
+
+}
---
+++ Unidad 2/U2Tarea2.java
@@ -0,0 +1,101 @@
+import javax.swing.*;
+/*
+    Este programa convierte una expresión de infijo a postfijo
+    Los comentarios son nomas para puro debug
+ */
+
+public class U2Tarea2 {
+    public static void main(String[] args) throws InterruptedException {
+        String res = "";
+        boolean entro = false;
+        String op = JOptionPane.showInputDialog(null,"Ingresa la operacion");
+        PilaChar pila = new PilaChar(op.length());
+        char c;
+        for (int i = 0; i < op.length(); i++) {
+            c = op.charAt(i);
+            /*System.out.println("res: "+ res);
+            System.out.println("Caracter que entra al switch: " + c);*/
+            //Thread.sleep(2000);
+            switch (prioridad(c)){
+                case 0:
+                    res+= c;
+                    //System.out.println(res);
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                   /* System.out.println("El caracter " + c +" Entro al caso 3");
+                    Thread.sleep(2000);*/
+                    if(!pila.estaVacia()){
+                      //  System.out.println("entro a !pila.estavacia");
+                        while(true){
+                            //System.out.println("Caracter" + c +" Entro a ciclo");
+                            if(prioridad(c) <= prioridad(pila.datoEnTope()) && !entro){
+                               // System.out.println("Entro a condicion pricoidad(c) <=");
+                                res+= pila.datoEnTope();
+                               // System.out.println("Res en if prio = " + res);
+                                pila.eliminar();
+                            }else{
+                                /*Thread.sleep(2000);
+                                System.out.println("Entro a else pila.insertar(C)");*/
+                                pila.insertar(c);
+                                break;
+                            }
+                        }
+                    }else{
+                        /*Thread.sleep(2000);
+                        System.out.println("Entro a else pila insertar");*/
+                        pila.insertar(c);
+                    }
+                    break;
+                case 4:
+                    /*System.out.println("Entro a caso 4");
+                    Thread.sleep(2000);*/
+                    System.out.println(res);
+                    if(!pila.estaVacia()){
+                        System.out.println("Entro a !estaVacia");
+                        //Thread.sleep(2000);
+                        if(c == '('){
+                            /*System.out.println("Entro a if'('");
+                            Thread.sleep(2000);*/
+                            pila.insertar(c);
+                        }else if(c == ')'){
+                            /*System.out.println("Entro a elif ')'");
+                            Thread.sleep(2000);*/
+                            while(true){
+                                //System.out.println("Ayuda");
+                                if(pila.datoEnTope() != '('){
+                                    res+=pila.eliminar();
+                                }else{
+                                    pila.eliminar();
+                                    entro = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }else{
+                        if(c == '('){
+                            pila.insertar(c);
+                            entro = true;
+                        }
+                    }
+                    break;
+            }
+        }
+        while(!pila.estaVacia()){
+            res+= pila.eliminar();
+        }
+        JOptionPane.showMessageDialog(null,res);
+    }
+
+    public static int prioridad(char op){
+        String[] operadores = {"+-","*/","^ ","()"};
+        for (int i = 0; i < operadores.length; i++) {
+            if(op == operadores[i].charAt(0) || op == (operadores[i].charAt(1))){
+                //A System.out.println(operadores[i].charAt(0));
+                return i+1;
+            }
+        }
+        return 0;
+    }
+}
